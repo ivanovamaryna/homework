aeb581d4de600b108ec317cd92392d39fd4c4159
tania-hromova <61333362+tania-hromova@users.noreply.github.com>
2021-02-02 00:06:52 +0200

    Add files via upload

---
+++ lesson1/SignNowLogin.java
@@ -0,0 +1,24 @@
+package lesson1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SignNowLogin {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://app.signnow.com/");
+        driver.findElement(By.cssSelector("[id='login']")).clear();
+        driver.findElement(By.cssSelector("[id='login']")).sendKeys("hromova.tetiana+sn@airslate.com");
+        driver.findElement(By.cssSelector("[id='pswd']")).clear();
+        driver.findElement(By.cssSelector("[id='pswd']")).sendKeys("1234qweR");
+        driver.findElement(By.cssSelector("[id='pswd']")).sendKeys(Keys.ENTER);
+       // driver.findElement(By.cssSelector("//*[@data-autotest='doc-list__card']/div[1]//*[@class='snr-doc-card__name']")).click();
+        WebElement element = driver.findElement(By.cssSelector("//*[@data-autotest='doc-list__card']/div[1]//*[@class='snr-doc-card__name']"));
+        System.out.println(element.getAttribute("a"));
+        // System.out.println(driver.getTitle());
+        // driver.quit();
+    }
+}
