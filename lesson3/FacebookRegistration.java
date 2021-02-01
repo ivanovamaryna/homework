a2ff0ce2ed93fe679cbd730252b4c51b4452b33e
tania-hromova <61333362+tania-hromova@users.noreply.github.com>
2021-02-02 00:16:54 +0200

    Add files via upload

---
+++ lesson3/FacebookRegistration.java
@@ -0,0 +1,23 @@
+package lesson3;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FacebookRegistration {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/r.php");
+        List<WebElement> elements = driver.findElements(By.cssSelector(".reg_form_box"));
+        for(WebElement element : elements)
+            System.out.println(element.getText());
+        driver.quit();
+
+
+
+
+    }
+}
---
+++ lesson3/FilmInfo.java
@@ -0,0 +1,18 @@
+package lesson3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FilmInfo {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.imdb.com/chart/top");
+        WebElement element1 = driver.findElement(By.cssSelector("//*[@data-caller-name='chart-top250movie']/tbody//tr[11]//td[@class='titleColumn']/a"));
+        WebElement element2 = driver.findElement(By.cssSelector("//*[@data-caller-name='chart-top250movie']/tbody//tr[11]//td[contains(@class,'imdbRating')]/*"));
+        System.out.println("Link "+element1.getAttribute("href")+ ", title "+ element1.getText() + ", rating "+ element2.getText());
+        driver.quit();
+    }
+}
