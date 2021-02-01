67af7f50a3a03bc649805e707c2176579762b480
tania-hromova <61333362+tania-hromova@users.noreply.github.com>
2021-02-02 00:07:44 +0200

    Add files via upload

---
+++ lesson2/TypesMatch.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+public class TypesMatch {
+    public static void main(String[] args) {
+        //int->char
+        int x = 57;
+        char y = (char) x;
+        System.out.println("int->char "+y);
+        //float-> char
+        float f = 78.0f;
+        char ff = (char)f;
+        System.out.println("float-> char "+ff);
+        //float->char->int
+        float q = 67.0f;
+        char w = (char)q;
+        int e = (int)w;
+        System.out.println("float->char->int "+e);
+    }
+}
